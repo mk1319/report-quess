9a22938a6eab7fe50fd549686deea08bc3b6da76
Mukesh Rajpurohit <mukeshrr1321@outlook.com>
2023-04-13 11:47:00 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./table.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
+  async function callApi() {
+    await axios
+      .get("http://api.tradebook.in.net/v1/user/refer-data-quess")
+      .then((res) => {
+        if (res.data) {
+          if (Array.isArray(res.data.data)) {
+            setTableData(res.data.data.reverse());
+          }
+        }
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button>Refresh</button>
+      <div className="table-container">
+        <div className="table-scroll">
+          <table>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((data, index) => (
+                <tr key={data.email}>
+                  <td>{index + 1}</td>
+                  <td>{data.first_name + " " + data.last_name}</td>
+                  <td>{data.email}</td>
+                  <td className="fixed-right">{data.mobile_number}</td>
+                  <td>{data.created_date?.split("T")[0]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
